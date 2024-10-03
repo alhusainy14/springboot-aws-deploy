f657928176aa16f99f8025838b814e24755edb12
alhusain <alhusainy@gmaill.com>
2024-10-03 10:04:24 -0500

    docker image

---
+++ src/main/java/com/example/springboot_aws_deploy/api.java
@@ -0,0 +1,20 @@
+package com.example.springboot_aws_deploy;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class api {
+    @GetMapping("/data")
+    public String getData() {
+        return "First message from alhusainy AWS Ecs";
+    }
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return "Second message from Alhusainy AWS Ecs";
+    }
+}
